8f312500b7337063b105aedc07fa4110e019e926
Rasa Cosmin <cosmin.rasa@freshbyteinc.com>
2017-05-03 10:44:16 +0300

    ex_11: get input

---
+++ ex11.rb
@@ -0,0 +1,8 @@
+print "how old are you?"
+age = gets.chomp
+print "how tall are you?"
+height = gets.chomp
+print "how much do yo weigh?"
+weigh = gets.chomp
+
+puts "age = #{age}, height = #{height}, weigh = #{weigh}"
