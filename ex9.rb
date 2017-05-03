8e177cd79f570c3f233b06a050fbd6fc1fc3174d
Rasa Cosmin <cosmin.rasa@freshbyteinc.com>
2017-05-03 07:25:08 +0300

    new exercices

---
+++ ex9.rb
@@ -0,0 +1,10 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul"
+puts "zilele saptamanii #{days}"
+puts "lunile anului #{months}"
+
+puts %q{
+  ce face?
+ bine tu?
+    asa si eu
+}
