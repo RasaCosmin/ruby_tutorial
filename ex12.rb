f6bcb1009f09606f0d08110297ce843b6cedf319
Rasa Cosmin <cosmin.rasa@freshbyteinc.com>
2017-05-03 11:02:56 +0300

    some inputs

---
+++ ex12.rb
@@ -0,0 +1,17 @@
+# print "da un nr: "
+# nr = gets.chomp.to_i
+# bigger = nr * 100
+# puts "bigger = #{bigger}"
+#
+# print "da alt numar: "
+# alt_nr = gets.chomp
+# nr = alt_nr.to_i
+#
+# smaller = nr / 100.0
+#
+# puts "smaller = #{smaller}"
+print "da un nr cu virgula: "
+nr = gets.chomp.to_f
+puts "#{nr}"
+percentage = nr * 10 / 100
+puts "percentage = #{percentage}"
---
+++ ex13.rb
@@ -0,0 +1,9 @@
+f, s, t = ARGV
+r = $stdin.gets.chomp
+puts "f = #{f}"
+puts "s = #{s}"
+puts "t = #{t}"
+puts "r = #{r}"
+e = gets.chomp
+
+puts "e = #{e}"
