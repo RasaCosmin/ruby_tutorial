41f24680950024bb2eeadf18cb1e08be733929ce
Rasa Cosmin <cosmin.rasa@freshbyteinc.com>
2017-05-03 11:19:02 +0300

    work with argv and input

---
+++ ex14.rb
@@ -0,0 +1,21 @@
+user  = ARGV.first
+prompt = "> "
+puts "salut #{user}"
+puts "vreau sa iti pun cateva intrebari :)"
+puts "iti place de mine #{user}?"
+print prompt
+likes = $stdin.gets.chomp
+
+puts "unde locuiesti #{user}"
+print prompt
+adresa = $stdin.gets.chomp
+
+puts "ce calc ai?"
+print prompt
+calc = $stdin.gets.chomp
+
+puts """
+ai zis '#{likes}' iti place de mine.
+locuiesti in #{adresa}
+si ai un calc #{calc}
+"""
